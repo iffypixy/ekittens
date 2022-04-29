adf73a299787405596d0f27362beea601ee12ef4
Ansat Jartybayev <ansatjr@gmail.com>
2022-04-29 21:01:13 +0600

    chore: add redux provider

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,15 +1,19 @@
 import * as React from "react";
 import {createRoot} from "react-dom/client";
 import {BrowserRouter} from "react-router-dom";
+import {Provider} from "react-redux";
 
 import {App} from "@app/application";
+import {store} from "@shared/lib/store";
 
 const element = document.getElementById("root") as Element;
 
 const root = createRoot(element);
 
 root.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>,
+  <Provider store={store}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </Provider>,
 );
