2f3285d62c219944c3d2d75581b9f9b04192cd64
Ansat Jartybayev <ansatjr@gmail.com>
2022-04-30 17:24:32 +0600

    chore: enable react-query

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -2,6 +2,8 @@ import * as React from "react";
 import {createRoot} from "react-dom/client";
 import {BrowserRouter} from "react-router-dom";
 import {Provider} from "react-redux";
+import {QueryClientProvider, QueryClient} from "react-query";
+import {ReactQueryDevtools} from "react-query/devtools";
 
 import {App} from "@app/application";
 import {store} from "@shared/lib/store";
@@ -10,10 +12,15 @@ import "@shared/lib/i18n";
 const element = document.getElementById("root") as Element;
 const root = createRoot(element);
 
+const queryClient = new QueryClient();
+
 root.render(
   <Provider store={store}>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <QueryClientProvider client={queryClient}>
+      <ReactQueryDevtools initialIsOpen={false} />
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </QueryClientProvider>
   </Provider>,
 );
