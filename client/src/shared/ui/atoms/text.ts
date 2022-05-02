5f155f304efdf4b9da1dc5fddca1bcea849df3ed
Ansat Jartybayev <ansatjr@gmail.com>
2022-05-02 23:05:02 +0600

    feat: add text component

---
+++ client/src/shared/ui/atoms/text.ts
@@ -0,0 +1,21 @@
+import {styled, css} from "@mui/material";
+
+interface TextProps {
+  uppercase?: boolean;
+}
+
+const propsNotToForward = ["uppercase"];
+const shouldForwardProp = (prop: string) => !propsNotToForward.includes(prop);
+
+export const Text = styled("span", {shouldForwardProp})<TextProps>`
+  color: #000000;
+  font-family: "Miriam Libre", sans-serif;
+  font-weight: 400;
+  font-size: 1.6rem;
+
+  ${({uppercase}) =>
+    uppercase &&
+    css`
+      text-transform: uppercase;
+    `}
+`;
