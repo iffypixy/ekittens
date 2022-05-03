29f061b63d8a0c20ccf68a7b80d016d9b47bc9a3
Ansat Jartybayev <ansatjr@gmail.com>
2022-05-03 19:16:41 +0600

    chore: add server repo

---
+++ server/.eslintrc.js
@@ -0,0 +1,23 @@
+module.exports = {
+  plugins: ["@typescript-eslint/eslint-plugin", "prettier"],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    project: "tsconfig.json",
+    sourceType: "module",
+  },
+  extends: [
+    "plugin:@typescript-eslint/recommended",
+    "plugin:prettier/recommended",
+  ],
+  root: true,
+  env: {
+    node: true,
+  },
+  ignorePatterns: [".eslintrc.js"],
+  rules: {
+    "@typescript-eslint/interface-name-prefix": "off",
+    "@typescript-eslint/explicit-function-return-type": "off",
+    "@typescript-eslint/explicit-module-boundary-types": "off",
+    "@typescript-eslint/no-explicit-any": "off",
+  },
+};
