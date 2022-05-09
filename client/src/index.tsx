94acc933afdfbf5fb0f38319b07753e2570c604c
Ansat Jartybayev <ansatjr@gmail.com>
2022-05-09 22:24:48 +0600

    chore: remove react-query setup

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -2,8 +2,6 @@ import * as React from "react";
 import {createRoot} from "react-dom/client";
 import {BrowserRouter} from "react-router-dom";
 import {Provider} from "react-redux";
-import {QueryClientProvider, QueryClient} from "react-query";
-import {ReactQueryDevtools} from "react-query/devtools";
 
 import {App} from "@app/application";
 import {store} from "@shared/lib/store";
@@ -12,15 +10,10 @@ import "@shared/lib/i18n";
 const element = document.getElementById("root") as Element;
 const root = createRoot(element);
 
-const queryClient = new QueryClient();
-
 root.render(
   <Provider store={store}>
-    <QueryClientProvider client={queryClient}>
-      <ReactQueryDevtools initialIsOpen={false} />
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
-    </QueryClientProvider>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </Provider>,
 );
