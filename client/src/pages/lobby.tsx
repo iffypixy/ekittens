1e8ffaf833dacaec0e56cb21208402585663a64d
Ansat Jartybayev <ansatjr@gmail.com>
2022-05-14 02:46:37 +0600

    chore: add lobby page

---
+++ client/src/pages/lobby.tsx
@@ -0,0 +1,17 @@
+import * as React from "react";
+
+import {
+  MainTemplate,
+  CenterTemplate,
+  FullScreenTemplate,
+} from "@shared/ui/templates";
+
+export const LobbyPage: React.FC = () => (
+  <MainTemplate>
+    <FullScreenTemplate>
+      <CenterTemplate>
+        <></>
+      </CenterTemplate>
+    </FullScreenTemplate>
+  </MainTemplate>
+);
