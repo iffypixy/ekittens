128e5949118065c1b9a13deec7327acb44f3bbfa
Ansat Jartybayev <ansatjr@gmail.com>
2022-05-14 02:48:04 +0600

    chore: add paths

--- client/src/pages/routes.tsx
+++ client/src/pages/routes.tsx
@@ -1,10 +1,16 @@
 import * as React from "react";
 import {Routes as Switch, Route} from "react-router-dom";
 
+import {HomePage} from "./home";
+import {LobbyPage} from "./lobby";
 import {MatchPage} from "./match";
+import {InvitePage} from "./invite";
 
 export const Routes: React.FC = () => (
   <Switch>
+    <Route path="/" element={<HomePage />} />
+    <Route path="/lobby" element={<LobbyPage />} />
+    <Route path="/invite/:id" element={<InvitePage />} />
     <Route path="/:id" element={<MatchPage />} />
   </Switch>
 );
