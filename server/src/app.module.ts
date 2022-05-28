39bb72231e46370bdd443c63638fc305da47c9bc
Ansat Jartybayev <ansatjr@gmail.com>
2022-05-28 14:03:42 +0600

    chore: add init

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,6 +1,30 @@
 import {Module} from "@nestjs/common";
+import {ConfigModule, ConfigService} from "@nestjs/config";
+import {BullModule} from "@nestjs/bull";
+
+import {redisConfig} from "@config/index";
+import {MatchModule} from "@modules/match";
+
+const env = process.env.NODE_ENV || "development";
 
 @Module({
-  imports: [],
+  imports: [
+    MatchModule,
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: `.env.${env}`,
+      load: [redisConfig],
+    }),
+    BullModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (service: ConfigService) => ({
+        redis: {
+          host: service.get("host"),
+          port: service.get("redis.port"),
+        },
+      }),
+    }),
+  ],
 })
 export class AppModule {}
--- server/src/main.ts
+++ server/src/main.ts
@@ -1,11 +1,20 @@
 import {NestFactory} from "@nestjs/core";
 
+import {session} from "@lib/session";
+import {ClusterService} from "@lib/cluster";
 import {AppModule} from "./app.module";
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, {
+    cors: {
+      credentials: true,
+      origin: process.env.CLIENT_ORIGIN,
+    },
+  });
 
-  await app.listen(3000);
+  app.use(session());
+
+  await app.listen(5000);
 }
 
-bootstrap();
+ClusterService.clusterize(bootstrap);
