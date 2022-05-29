2e2fa045f693de70bcaa308cdd923a21f2b9d167
Ansat Jartybayev <ansatjr@gmail.com>
2022-05-29 16:30:45 +0600

    chore: add input

---
+++ client/src/shared/ui/atoms/input.ts
@@ -0,0 +1,11 @@
+import {InputBase, styled} from "@mui/material";
+
+export const Input = styled(InputBase)`
+  color: #000000;
+  font-family: "Bungee", sans-serif;
+  font-weight: 400;
+  font-size: 1.4rem;
+  border: 0.25rem solid #000000;
+  border-radius: 0.5rem;
+  padding: 0.75rem 1.5rem;
+`;
