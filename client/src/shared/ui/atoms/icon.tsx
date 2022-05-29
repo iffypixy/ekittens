5ceb0bd6c4b9ab63c6c4e471f7917ef3d42a2aa5
Ansat Jartybayev <ansatjr@gmail.com>
2022-05-30 00:10:16 +0600

    chore: add icon

--- client/src/shared/ui/atoms/icon.tsx
+++ client/src/shared/ui/atoms/icon.tsx
@@ -6,7 +6,7 @@ interface StyledProps {
 }
 
 interface IconProps extends StyledProps {
-  name: "reset";
+  name: "reset" | "crown";
 }
 
 export const Icon: React.FC<IconProps> = ({name, ...props}) => {
@@ -22,6 +22,16 @@ export const Icon: React.FC<IconProps> = ({name, ...props}) => {
         </SVG>
       );
 
+    case "crown":
+      return (
+        <SVG viewBox="0 0 32 32" {...props}>
+          <g>
+            <path d="m27.488 23-.513 2.225c-.105.454-.509.775-.975.775h-20c-.466 0-.87-.321-.975-.775l-.513-2.225z"></path>
+            <path d="m29.975 12.225-2.025 8.775h-23.9l-2.025-8.775c-.089-.387.059-.791.378-1.028.32-.237.749-.262 1.093-.065l6.189 3.537 5.482-8.223c.179-.268.475-.434.796-.446.324-.014.629.132.826.386l6.429 8.266 5.227-3.484c.341-.226.786-.224 1.123.009.338.233.498.649.407 1.048z"></path>
+          </g>
+        </SVG>
+      );
+
     default:
       return null;
   }
@@ -30,5 +40,4 @@ export const Icon: React.FC<IconProps> = ({name, ...props}) => {
 const SVG = styled("svg")`
   display: block;
   width: 2rem;
-  height: 2rem;
 `;
