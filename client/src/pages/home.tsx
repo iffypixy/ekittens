9e14f220719c0d5b0bd679b7c9232f86f0844fef
Ansat Jartybayev <ansatjr@gmail.com>
2022-05-29 16:31:54 +0600

    feat: implement home page

--- client/src/pages/home.tsx
+++ client/src/pages/home.tsx
@@ -1,17 +1,36 @@
 import * as React from "react";
+import {styled} from "@mui/material";
 
-import {
-  MainTemplate,
-  CenterTemplate,
-  FullScreenTemplate,
-} from "@shared/ui/templates";
+import {Button, Input, Text} from "@shared/ui/atoms";
+import {CenterTemplate, FullScreenTemplate} from "@shared/ui/templates";
+import {Col, Row} from "@shared/lib/layout";
+import {AvatarPicker} from "@entities/avatar";
 
 export const HomePage: React.FC = () => (
-  <MainTemplate>
-    <FullScreenTemplate>
-      <CenterTemplate>
-        <></>
-      </CenterTemplate>
-    </FullScreenTemplate>
-  </MainTemplate>
+  <FullScreenTemplate>
+    <CenterTemplate>
+      <Wrapper gap={6}>
+        <Row gap={5} align="center">
+          <AvatarPicker />
+
+          <Col gap={3}>
+            <Text uppercase>Choose an avatar and a username</Text>
+
+            <Input placeholder="best_player228" />
+          </Col>
+        </Row>
+
+        <Row w="100%" justify="center">
+          <Button>Start</Button>
+        </Row>
+      </Wrapper>
+    </CenterTemplate>
+  </FullScreenTemplate>
 );
+
+const Wrapper = styled(Col)`
+  background-color: #ffffff;
+  box-shadow: 0 0 0 1px rgba(0, 0, 0, 0.05);
+  border-radius: 2rem;
+  padding: 5rem;
+`;
