455ce4a28b0971053fc8091968cce2bd8db42ad6
Ansat Jartybayev <ansatjr@gmail.com>
2022-05-30 00:09:14 +0600

    chore: change api export

--- client/src/shared/api/index.ts
+++ client/src/shared/api/index.ts
@@ -1,1 +1,12 @@
+export {lobbyApi, lobbyEvents} from "./lobby.api";
+export type {
+  CreateLobbyData,
+  CreateLobbyOutput,
+  JoinLobbyData,
+  JoinLobbyOutput,
+  KickLobbyPlayerData,
+  LeaveLobbyData,
+  Lobby,
+  LobbyPlayer,
+} from "./lobby.api";
 export {matchApi} from "./match.api";
