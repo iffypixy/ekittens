650577e56f41c79563d34099928ec7c6e8047f5a
Ansat Jartybayev <ansatjr@gmail.com>
2022-05-29 23:59:29 +0600

    chore: correct full-screen template

--- client/src/shared/ui/templates/full-screen.tsx
+++ client/src/shared/ui/templates/full-screen.tsx
@@ -1,18 +1,22 @@
 import * as React from "react";
 import {styled} from "@mui/material";
 
+import {MainTemplate} from "@shared/ui/templates";
+import {Row} from "@shared/lib/layout";
+
 interface FullTemplateProps {
   children: React.ReactNode;
 }
 
 export const FullScreenTemplate: React.FC<FullTemplateProps> = ({children}) => (
-  <Wrapper>{children}</Wrapper>
+  <MainTemplate>
+    <Wrapper align="center" justify="center">
+      {children}
+    </Wrapper>
+  </MainTemplate>
 );
 
-const Wrapper = styled("div")`
+const Wrapper = styled(Row)`
   width: 100%;
   height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
 `;
