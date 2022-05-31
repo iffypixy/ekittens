1bd68149bdeda77905fb994890aed8df8b0db219
Ansat Jartybayev <ansatjr@gmail.com>
2022-06-01 00:18:55 +0600

    chore: update match plain

--- server/src/modules/match/lib/plain.ts
+++ server/src/modules/match/lib/plain.ts
@@ -2,17 +2,19 @@ import {Match, MatchPublic} from "./typings";
 
 export const plain = {
   match: (match: Match): MatchPublic => {
-    const {id, deck, pile, players} = match;
+    const {id, deck, pile, players, turn} = match;
 
     const mapped = players.map((player) => ({
       id: player.id,
       username: player.username,
       avatar: player.avatar,
+      left: player.cards.length,
     }));
 
     return {
       id,
       pile,
+      turn,
       left: deck.length,
       players: mapped,
     };
