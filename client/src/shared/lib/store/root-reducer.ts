6382407298a8685e98dbfb72e4645e1f5d0dd959
Ansat Jartybayev <ansatjr@gmail.com>
2022-06-01 00:13:51 +0600

    chore: update root-reducer

--- client/src/shared/lib/store/root-reducer.ts
+++ client/src/shared/lib/store/root-reducer.ts
@@ -1,7 +1,9 @@
 import {combineReducers} from "@reduxjs/toolkit";
 
 import {lobbyModel} from "@features/lobby";
+import {matchModel} from "@features/match";
 
 export const rootReducer = combineReducers({
   lobby: lobbyModel.reducer,
+  match: matchModel.reducer,
 });
