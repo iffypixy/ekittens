882ca6a2997af933be0596585689f27d85b9e870
Ansat Jartybayev <ansatjr@gmail.com>
2022-06-01 00:19:36 +0600

    chore: update match module

--- server/src/modules/match/match.module.ts
+++ server/src/modules/match/match.module.ts
@@ -1,4 +1,48 @@
 import {Module} from "@nestjs/common";
+import {BullModule} from "@nestjs/bull";
 
-@Module({})
+import {MatchGateway} from "./match.gateway";
+
+@Module({
+  imports: [
+    BullModule.registerQueue(
+      {
+        name: "inactive",
+        defaultJobOptions: {
+          removeOnComplete: true,
+          removeOnFail: true,
+        },
+      },
+      {
+        name: "explosion",
+        defaultJobOptions: {
+          removeOnComplete: true,
+          removeOnFail: true,
+        },
+      },
+      {
+        name: "favor-response",
+        defaultJobOptions: {
+          removeOnComplete: true,
+          removeOnFail: true,
+        },
+      },
+      {
+        name: "play",
+        defaultJobOptions: {
+          removeOnComplete: true,
+          removeOnFail: true,
+        },
+      },
+      {
+        name: "spot-response",
+        defaultJobOptions: {
+          removeOnComplete: true,
+          removeOnFail: true,
+        },
+      },
+    ),
+  ],
+  providers: [MatchGateway],
+})
 export class MatchModule {}
