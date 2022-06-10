d217ddaded6bc879bfed644e77544bf834be3b34
Ansat Jartybayev <ansatjr@gmail.com>
2022-06-10 23:40:47 +0600

    feat: add ws

--- client/src/shared/lib/websocket/index.ts
+++ client/src/shared/lib/websocket/index.ts
@@ -1,7 +1,33 @@
 import {io} from "socket.io-client";
 
-// https://api.ekittens.lol
-
-export const socket = io("https://api.ekittens.lol", {
+export const socket = io(process.env.BACKEND_URL, {
   withCredentials: true,
 });
+
+interface WsResponse<T> {
+  ok: boolean;
+  msg: string;
+  payload: T;
+}
+
+const timeout = 5000;
+
+const INTERNAL_SERVER_ERROR = "Something went wrong...";
+
+export const ws = {
+  emit: <R>(event: string, payload: any) =>
+    new Promise<R>((resolve, reject) => {
+      socket
+        .timeout(timeout)
+        .emit(event, payload, (error: Error, response: WsResponse<R>) => {
+          if (error) reject(INTERNAL_SERVER_ERROR);
+
+          if (response.ok) resolve(response.payload);
+          else reject(response.msg);
+        });
+    }),
+  on: (event: string, listener: (...args: any[]) => void) => {
+    socket.off(event, listener);
+    socket.on(event, listener);
+  },
+};
