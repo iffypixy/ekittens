70b17f92f8c3270358201d84fadc24d8eaffee26
Ansat Jartybayev <ansatjr@gmail.com>
2022-06-12 23:50:31 +0600

    chore: update cluster lib

--- server/src/lib/cluster/index.ts
+++ server/src/lib/cluster/index.ts
@@ -1,31 +1,26 @@
-import {Injectable} from "@nestjs/common";
 import cluster from "cluster";
 import os from "os";
-import bull from "bull";
 
-@Injectable()
-export class ClusterService {
-  static clusterize(callback: Function): void {
-    const isProd = process.env.NODE_ENV === "production";
+export const clusterize = (callback: Function): void => {
+  const isProd = process.env.NODE_ENV === "production";
 
-    if (cluster.isPrimary) {
-      console.log(`Primary ${process.pid} is running`);
+  if (cluster.isPrimary && isProd) {
+    console.log(`Primary ${process.pid} is running`);
 
-      const cpus = os.cpus().length;
+    const cpus = os.cpus().length;
 
-      for (let i = 0; i < cpus; i++) {
-        cluster.fork();
-      }
-
-      cluster.on("exit", (worker) => {
-        console.log(`Worker ${worker.process.pid} died. Restarting`);
-
-        cluster.fork();
-      });
-    } else {
-      console.log(`Worker ${process.pid} is running`);
-
-      callback();
+    for (let i = 0; i < cpus; i++) {
+      cluster.fork();
     }
+
+    cluster.on("exit", (worker) => {
+      console.log(`Worker ${worker.process.pid} died. Restarting`);
+
+      cluster.fork();
+    });
+  } else {
+    console.log(`Worker ${process.pid} is running`);
+
+    callback();
   }
-}
+};
