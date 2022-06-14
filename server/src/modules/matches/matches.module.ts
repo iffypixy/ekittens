1e49c472a531c0b449a3ef734c8a642105fc94fe
Ansat Jartybayev <ansatjr@gmail.com>
2022-06-14 23:16:44 +0600

    chore: update matches module

--- server/src/modules/matches/matches.module.ts
+++ server/src/modules/matches/matches.module.ts
@@ -1,9 +1,21 @@
 import {Module} from "@nestjs/common";
 import {TypeOrmModule} from "@nestjs/typeorm";
+import {BullModule} from "@nestjs/bull";
 
 import {Match, MatchParticipant} from "./entities";
 
+const jobOptions = {
+  removeOnComplete: true,
+  removeOnFail: true,
+};
+
 @Module({
-  imports: [TypeOrmModule.forFeature([Match, MatchParticipant])],
+  imports: [
+    TypeOrmModule.forFeature([Match, MatchParticipant]),
+    BullModule.registerQueue({
+      name: "matchmaking",
+      defaultJobOptions: jobOptions,
+    }),
+  ],
 })
 export class MatchesModule {}
