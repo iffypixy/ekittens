ef74bab4138906aee9fef9c024aa8b907ff275b7
Ansat Jartybayev <ansatjr@gmail.com>
2022-06-15 23:57:46 +0600

    chore: update typings of matches

--- server/src/modules/matches/lib/typings.ts
+++ server/src/modules/matches/lib/typings.ts
@@ -1,11 +1,26 @@
 import {User, UserPublic} from "@modules/users";
+import {Card} from "./deck";
+
+export interface OngoingMatchParticipant extends User {
+  cards: Card[];
+}
 
 export interface OngoingMatch {
   id: string;
-  participants: User[];
+  participants: OngoingMatchParticipant[];
+  deck: Card[];
+  pile: Card[];
+  turn: number;
+  context: {
+    nope: boolean;
+    attacks: number;
+  };
 }
+
+export interface OngoingMatchParticipantPublic extends UserPublic {}
 
 export interface OngoingMatchPublic {
   id: string;
-  participants: UserPublic[];
+  participants: OngoingMatchParticipantPublic[];
+  pile: Card[];
 }
