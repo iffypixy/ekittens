643b1ae922876db7391fbcd8d8abc2e9f90509b7
Ansat Jartybayev <ansatjr@gmail.com>
2022-06-15 23:59:15 +0600

    chore: update lib/deck of matches

---
+++ server/src/modules/matches/lib/match.ts
@@ -0,0 +1,10 @@
+import {OngoingMatch} from "./typings";
+
+const changeTurn = (match: OngoingMatch) => {
+  const next = match.participants[match.turn + 1];
+
+  if (!!next) match.turn++;
+  else match.turn = 0;
+};
+
+export const match = {changeTurn};
