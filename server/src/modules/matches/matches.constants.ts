bbfa6cb5aebe8ec1cc6ffdd78d9a85a714c56e2d
Ansat Jartybayev <ansatjr@gmail.com>
2022-06-16 22:29:02 +0600

    chore: update matches constants

--- server/src/modules/matches/matches.constants.ts
+++ server/src/modules/matches/matches.constants.ts
@@ -1,1 +1,21 @@
-export const MAX_MATCH_PLAYERS = 10;
+export const MATCH_PLAYERS_NUMBER = {
+  MIN: 2,
+  MAX: 10,
+};
+
+export const JOB_DELAY = {
+  INACTIVE: 30000,
+  EXPLODING_KITTEN_DEFUSE: 10000,
+  EXPLODING_KITTEN_INSERTION: 10000,
+  FAVOR_RESPONSE: 15000,
+  CARD_REPEAL: 5000,
+};
+
+export const QUEUES = {
+  MATCHMAKING: "matchmaking",
+  CARD_ACTION: "card-action",
+  INACTIVE: "inactive",
+  FAVOR: "favor",
+  EXPLODING_KITTEN_DEFUSE: "exploding-kitten-defuse",
+  EXPLODING_KITTEN_INSERTION: "exploding-kitten-insertion",
+};
