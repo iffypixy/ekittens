b44158edb3f1c861f0868d501061272c736dfada
Ansat Jartybayev <ansatjr@gmail.com>
2022-06-16 22:30:13 +0600

    chore: update typings

--- server/src/modules/matches/lib/typings.ts
+++ server/src/modules/matches/lib/typings.ts
@@ -8,9 +8,11 @@ export interface OngoingMatchPlayer extends User {
 export interface OngoingMatch {
   id: string;
   players: OngoingMatchPlayer[];
+  out: OngoingMatchPlayer[];
   deck: Card[];
   pile: Card[];
   turn: number;
+  playedBy: OngoingMatchPlayer["id"] | null;
   context: {
     nope: boolean;
     attacks: number;
@@ -23,4 +25,29 @@ export interface OngoingMatchPublic {
   id: string;
   players: OngoingMatchPlayerPublic[];
   pile: Card[];
+}
+
+export interface CardActionQueuePayload {
+  matchId: string;
+  card: Card;
+  payload: Partial<{
+    playerId: string;
+  }>;
+}
+
+export interface InactiveQueuePayload {
+  matchId: string;
+}
+
+export interface FavorQueuePayload {
+  matchId: string;
+  playerId: string;
+}
+
+export interface ExplodingKittenDefusePayload {
+  matchId: string;
+}
+
+export interface ExplodingKittenInsertionPayload {
+  matchId: string;
 }
