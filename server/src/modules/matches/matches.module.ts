a88959d8e1869f473b83b2fc277e5b7140f1d0bd
Ansat Jartybayev <ansatjr@gmail.com>
2022-06-16 22:31:19 +0600

    chore: update matches module

--- server/src/modules/matches/matches.module.ts
+++ server/src/modules/matches/matches.module.ts
@@ -1,21 +1,37 @@
 import {Module} from "@nestjs/common";
 import {TypeOrmModule} from "@nestjs/typeorm";
-import {BullModule} from "@nestjs/bull";
+import {BullModule, BullModuleOptions} from "@nestjs/bull";
 
+import {MatchmakingGateway} from "./gateways";
 import {Match, MatchPlayer} from "./entities";
+import {QUEUES} from "./matches.constants";
 
 const jobOptions = {
   removeOnComplete: true,
   removeOnFail: true,
 };
 
+const queues = [
+  QUEUES.MATCHMAKING,
+  QUEUES.CARD_ACTION,
+  QUEUES.INACTIVE,
+  QUEUES.FAVOR,
+  QUEUES.EXPLODING_KITTEN_DEFUSE,
+  QUEUES.EXPLODING_KITTEN_INSERTION,
+];
+
 @Module({
   imports: [
     TypeOrmModule.forFeature([Match, MatchPlayer]),
-    BullModule.registerQueue({
-      name: "matchmaking",
-      defaultJobOptions: jobOptions,
-    }),
+    BullModule.registerQueue(
+      ...queues.map(
+        (queue): BullModuleOptions => ({
+          name: queue,
+          defaultJobOptions: jobOptions,
+        }),
+      ),
+    ),
   ],
+  providers: [MatchmakingGateway],
 })
 export class MatchesModule {}
