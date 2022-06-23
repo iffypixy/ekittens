91e944792b51ed93687927df6f811fba545df412
Ansat Jartybayev <ansatjr@gmail.com>
2022-06-23 23:57:33 +0600

    chore: update ws helper

--- server/src/lib/ws/ws.helper.ts
+++ server/src/lib/ws/ws.helper.ts
@@ -3,6 +3,10 @@ import {Server, Socket} from "socket.io";
 export class WsHelper {
   constructor(private readonly server: Server) {}
 
+  getAllSockets(): Socket[] {
+    return Array.from(this.server.of("/").sockets.values());
+  }
+
   getSocketsByUserId(id: string): Socket[] {
     const sockets = Array.from(this.server.of("/").sockets.values());
 
