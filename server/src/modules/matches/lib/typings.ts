9cfc1f76c6eef8ac530b191534f98980206f36ad
Ansat Jartybayev <ansatjr@gmail.com>
2022-06-28 00:19:06 +0600

    chore: update typings lib of matches module

--- server/src/modules/matches/lib/typings.ts
+++ server/src/modules/matches/lib/typings.ts
@@ -1,23 +1,52 @@
 import {User, UserPublic} from "@modules/users";
-import {Card, Combo} from "./deck";
+
+export type Card =
+  | "exploding-kitten"
+  | "defuse"
+  | "attack"
+  | "nope"
+  | "shuffle"
+  | "skip"
+  | "see-the-future-3x"
+  | "targeted-attack"
+  | "alter-the-future-3x"
+  | "draw-from-the-bottom"
+  | "imploding-kitten-open"
+  | "imploding-kitten-closed"
+  | "reverse"
+  | "streaking-kitten"
+  | "super-skip"
+  | "see-the-future-5x"
+  | "swap-top-and-bottom"
+  | "catomic-bomb"
+  | "mark"
+  | "bury"
+  | "personal-attack"
+  | "share-the-future-3x";
 
 export interface OngoingMatchPlayer extends User {
   cards: Card[];
+  marked: number[];
 }
 
 export interface OngoingMatch {
   id: string;
   players: OngoingMatchPlayer[];
   out: OngoingMatchPlayer[];
-  deck: Card[];
-  pile: Card[];
+  draw: Card[];
+  discard: Card[];
   turn: number;
-  playedBy: OngoingMatchPlayer["id"] | null;
   votes: {
     skip: OngoingMatchPlayer["id"][];
   };
+  status: {
+    type: MatchStatusType;
+    at: number;
+    payload?: any;
+  };
   context: {
-    nope: boolean;
+    noped: boolean;
+    reversed: boolean;
     attacks: number;
   };
 }
@@ -27,46 +56,40 @@ export interface OngoingMatchPlayerPublic extends UserPublic {}
 export interface OngoingMatchPublic {
   id: string;
   players: OngoingMatchPlayerPublic[];
-  pile: Card[];
+  out: OngoingMatchPlayerPublic[];
+  discard: Card[];
+  turn: number;
+  votes: {
+    skip: OngoingMatchPlayerPublic["id"][];
+  };
+  status: {
+    type: MatchStatusType;
+    at: number;
+    payload?: any;
+  };
+  context: {
+    noped: boolean;
+    reversed: boolean;
+    attacks: number;
+  };
 }
 
 export interface CardActionQueuePayload {
   matchId: string;
   card: Card;
-  payload: Partial<{
-    playerId: string;
-  }>;
+  payload: any;
 }
 
-export interface ComboActionQueuePayload {
-  matchId: string;
-  combo: Combo;
-  playerId: string;
-  card: Card;
-  payload: Partial<{
-    chosenIndex: number;
-    different: Card[];
-  }>;
-}
-
-export interface InactiveQueuePayload {
+export interface InactivityQueuePayload {
   matchId: string;
 }
 
-export interface FavorQueuePayload {
-  matchId: string;
-  playerId: string;
-}
-
-export interface ExplodingKittenDefusePayload {
-  matchId: string;
-  addedAt: number;
-}
-
-export interface ExplodingKittenInsertionPayload {
-  matchId: string;
-}
-
-export interface PileCardDrawPayload {
-  matchId: string;
-}
+export type MatchStatusType =
+  | "exploding-kitten-defuse"
+  | "exploding-kitten-insertion"
+  | "future-cards-alter"
+  | "future-cards-share"
+  | "card-bury"
+  | "imploding-kitten-insertion"
+  | "action-delay"
+  | "waiting-for-action";
