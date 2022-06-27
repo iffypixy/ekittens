7df6ef8b82d16b223740769974ef6a23385323d4
Ansat Jartybayev <ansatjr@gmail.com>
2022-06-28 00:17:06 +0600

    feat: add constants lib to matches module

---
+++ server/src/modules/matches/lib/constants.ts
@@ -0,0 +1,35 @@
+import {utils} from "@lib/utils";
+import {MatchStatusType} from "./typings";
+
+export const NUMBER_OF_MATCH_PLAYERS = {
+  MIN: 2,
+  MAX: 10,
+};
+
+export const QUEUE = {
+  MATCHMAKING: {
+    NAME: "matchmaking",
+    REPEAT: 5000,
+  },
+  CARD_ACTION: {
+    NAME: "card-action",
+    DELAY: 5000,
+  },
+  INACTIVITY: {
+    NAME: "inactivity",
+    DELAY: 45000,
+  },
+};
+
+export const NUMBER_OF_INITIAL_CARDS = 4;
+
+export const MATCH_STATUS = utils.AssertRecordType<MatchStatusType>()({
+  EXPLODING_KITTEN_DEFUSE: "exploding-kitten-defuse",
+  EXPLODING_KITTEN_INSERTION: "exploding-kitten-insertion",
+  FUTURE_CARDS_ALTER: "future-cards-alter",
+  FUTURE_CARDS_SHARE: "future-cards-share",
+  CARD_BURY: "card-bury",
+  IMPLODING_KITTEN_INSERTION: "imploding-kitten-insertion",
+  WAITING_FOR_ACTION: "waiting-for-action",
+  ACTION_DELAY: "action-delay",
+});
