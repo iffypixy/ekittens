7e12cb1bd25bd461ade8c56fd7c903291376f085
Ansat Jartybayev <ansatjr@gmail.com>
2022-06-28 00:18:45 +0600

    chore: update deck lib of matches module

--- server/src/modules/matches/lib/deck.ts
+++ server/src/modules/matches/lib/deck.ts
@@ -1,100 +1,49 @@
-import {shuffle} from "@lib/utils";
-import {INITIAL_CARDS_QUANTITY} from "../matches.constants";
+import {utils} from "@lib/utils";
+import {NUMBER_OF_INITIAL_CARDS} from "./constants";
+import {Card} from "./typings";
 
-export type Card =
-  | "exploding-kitten"
-  | "defuse"
-  | "favor"
-  | "attack"
-  | "nope"
-  | "shuffle"
-  | "skip"
-  | "see-the-future"
-  | "beard-cat"
-  | "cattermelon"
-  | "rainbow-ralphing-cat"
-  | "tacocat"
-  | "hairy-potato-cat";
-
-export type Combo =
-  | "two-of-a-kind"
-  | "three-of-a-kind"
-  | "five-different-cards";
-
-const cards: {
-  all: Card[];
-  special: Card[];
-  ordinary: Card[];
-  default: Card[];
-} = {
-  all: [
-    "exploding-kitten",
-    "defuse",
-    "favor",
-    "attack",
-    "nope",
-    "shuffle",
-    "skip",
-    "see-the-future",
-    "beard-cat",
-    "cattermelon",
-    "rainbow-ralphing-cat",
-    "tacocat",
-    "hairy-potato-cat",
-  ],
-  special: ["exploding-kitten", "defuse"],
-  ordinary: [
-    "favor",
-    "attack",
-    "nope",
-    "shuffle",
-    "skip",
-    "see-the-future",
-    "beard-cat",
-    "cattermelon",
-    "rainbow-ralphing-cat",
-    "tacocat",
-    "hairy-potato-cat",
-  ],
-  default: [
-    "defuse",
-    "favor",
-    "attack",
-    "nope",
-    "shuffle",
-    "skip",
-    "see-the-future",
-    "beard-cat",
-    "cattermelon",
-    "rainbow-ralphing-cat",
-    "tacocat",
-    "hairy-potato-cat",
-  ],
-};
-
-const combos: Combo[] = [
-  "two-of-a-kind",
-  "three-of-a-kind",
-  "five-different-cards",
+const cards: Card[] = [
+  "exploding-kitten",
+  "defuse",
+  "attack",
+  "nope",
+  "shuffle",
+  "skip",
+  "see-the-future-3x",
+  "targeted-attack",
+  "alter-the-future-3x",
+  "draw-from-the-bottom",
+  "imploding-kitten-open",
+  "imploding-kitten-closed",
+  "reverse",
+  "streaking-kitten",
+  "super-skip",
+  "see-the-future-5x",
+  "swap-top-and-bottom",
+  "catomic-bomb",
+  "mark",
+  "bury",
+  "personal-attack",
+  "share-the-future-3x",
 ];
 
-const init = (players: number) => {
+const generate = (players: number) => {
   const total = players * 10;
 
-  const deck = new Array(Math.ceil(total / cards.ordinary.length))
-    .fill(shuffle(cards.ordinary))
+  const deck = new Array(Math.ceil(total / cards.length))
+    .fill(utils.shuffleArray(cards))
     .flat();
 
   deck.length = total;
 
-  const shuffled = shuffle<Card>(deck);
+  const shuffled = utils.shuffleArray<Card>(deck);
 
   const individual: Card[][] = Array.from<[], Card[]>(
     new Array(players),
     () => ["defuse"],
   );
 
-  const CARDS_TO_RANDOMIZE = INITIAL_CARDS_QUANTITY - 1;
+  const CARDS_TO_RANDOMIZE = NUMBER_OF_INITIAL_CARDS - 1;
 
   for (let i = 0; i < players; i++) {
     for (let j = 0; j < CARDS_TO_RANDOMIZE; j++) {
@@ -104,7 +53,7 @@ const init = (players: number) => {
     }
   }
 
-  shuffled.splice(0, players * INITIAL_CARDS_QUANTITY);
+  shuffled.splice(0, players * NUMBER_OF_INITIAL_CARDS);
 
   const TOTAL_EXPLODING_KITTEN_CARDS = players - 1;
   const TOTAL_DEFUSE_CARDS = players * 2;
@@ -118,9 +67,9 @@ const init = (players: number) => {
   shuffled.push(...defuse);
 
   return {
-    individual: individual.map((cards) => shuffle(cards)),
-    main: shuffle(shuffled),
+    individual: individual.map((cards) => utils.shuffleArray(cards)),
+    main: utils.shuffleArray(shuffled),
   };
 };
 
-export const deck = {init, cards, combos};
+export const deck = {generate, cards};
