1ad4929250f3ace357a4631cfd1f27e2fa961add
Ansat Jartybayev <ansatjr@gmail.com>
2022-06-28 00:26:19 +0600

    chore: update export of utils

--- server/src/lib/utils/index.ts
+++ server/src/lib/utils/index.ts
@@ -1,5 +1,4 @@
 import {shuffleArray} from "./shuffle-array";
-import {removeElementFromArray} from "./remove-element-from-array";
 import {splitArrayIntoChunks} from "./split-into-chunks";
 import {AssertRecordType} from "./assert-record-type";
 
