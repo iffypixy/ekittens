db0c7d8baaf96a6a963399411ef6a6a1986a30dc
Ansat Jartybayev <ansatjr@gmail.com>
2022-06-28 17:11:51 +0600

    feat: add elo lib

---
+++ server/src/lib/elo/index.ts
@@ -0,0 +1,45 @@
+const PERF = 400;
+const K_FACTOR = 32;
+
+const SCORE = {
+  VICTORY: 1,
+  DEFEAT: 0,
+};
+
+const adjust = (shift: number, opponents: number) => shift * opponents;
+
+const calculate = (
+  rating: number,
+  opponents: number[],
+  score: number,
+): number => {
+  const average = Math.round(
+    opponents.reduce((prev, rating) => prev + rating, 0) / opponents.length,
+  );
+
+  const ratingDiff = rating - average;
+
+  const expected = 1 / (1 + Math.pow(10, ratingDiff / PERF));
+
+  const scoreDiff = score - expected;
+
+  let shift = K_FACTOR * scoreDiff;
+
+  if (score === SCORE.VICTORY) shift = adjust(shift, opponents.length);
+
+  const updated = Math.round(rating + shift);
+
+  return updated;
+};
+
+const ifWon = (rating: number, opponents: number[]): number =>
+  calculate(rating, opponents, SCORE.VICTORY);
+
+const ifLost = (rating: number, opponents: number[]): number =>
+  calculate(rating, opponents, SCORE.DEFEAT);
+
+export const elo = {
+  ifWon,
+  ifLost,
+  adjust,
+};
