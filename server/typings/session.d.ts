58471f6efc48311820426cb7700b9531c9d8c897
Ansat Jartybayev <ansatjr@gmail.com>
2022-06-28 17:12:27 +0600

    chore: update "users" to "user"

--- server/typings/session.d.ts
+++ server/typings/session.d.ts
@@ -1,6 +1,6 @@
 import "express-session";
 
-import {User} from "@modules/users";
+import {User} from "@modules/user";
 
 declare module "express-session" {
   export interface Sess extends Session {
