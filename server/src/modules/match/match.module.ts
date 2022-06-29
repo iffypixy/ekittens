df7c17354278ded8bb98d39097518966d5f2dd08
Ansat Jartybayev <ansatjr@gmail.com>
2022-06-29 13:42:11 +0600

    chore: update match module

--- server/src/modules/match/match.module.ts
+++ server/src/modules/match/match.module.ts
@@ -1,9 +1,11 @@
-import {Module} from "@nestjs/common";
+import {MiddlewareConsumer, Module, NestModule} from "@nestjs/common";
 import {TypeOrmModule} from "@nestjs/typeorm";
 import {BullModule, BullModuleOptions} from "@nestjs/bull";
 import Bull from "bull";
 
 import {UserModule} from "@modules/user";
+import {AuthMiddleware} from "@modules/auth";
+import {MatchController} from "./controllers";
 import {MatchPlayerService, MatchService} from "./services";
 import {MatchGateway, PublicMatchGateway} from "./gateways";
 import {Match, MatchPlayer} from "./entities";
@@ -35,5 +37,10 @@ const queues = [...Object.values(QUEUE)].map((queue) => queue.NAME);
     MatchService,
     MatchPlayerService,
   ],
+  controllers: [MatchController],
 })
-export class MatchModule {}
+export class MatchModule implements NestModule {
+  configure(consumer: MiddlewareConsumer): void {
+    consumer.apply(AuthMiddleware).forRoutes(MatchController);
+  }
+}
