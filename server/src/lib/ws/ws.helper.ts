7a1d2f15d39eae00305cc34370bf6fee12eec603
Ansat Jartybayev <ansatjr@gmail.com>
2022-06-29 12:03:42 +0600

    chore: update ws lib

--- server/src/lib/ws/ws.helper.ts
+++ server/src/lib/ws/ws.helper.ts
@@ -3,13 +3,27 @@ import {Server, Socket} from "socket.io";
 export class WsHelper {
   constructor(private readonly server: Server) {}
 
-  getAllSockets(): Socket[] {
-    return Array.from(this.server.of("/").sockets.values());
+  getSocketsInRoomByUserId(room: string, id: string): Socket[] {
+    const namespace = this.server.of("/");
+
+    const ids = Array.from(namespace.adapter.rooms.get(room));
+
+    const sockets = ids.map(this.getSocketById);
+
+    return sockets.filter((socket) => socket.request.session.user.id === id);
   }
 
   getSocketsByUserId(id: string): Socket[] {
-    const sockets = Array.from(this.server.of("/").sockets.values());
+    const namespace = this.server.of("/");
+
+    const sockets = Array.from(namespace.sockets.values());
 
     return sockets.filter((socket) => socket.request.session.user.id === id);
+  }
+
+  getSocketById(id: string): Socket {
+    const namespace = this.server.of("/");
+
+    return namespace.sockets.get(id);
   }
 }
