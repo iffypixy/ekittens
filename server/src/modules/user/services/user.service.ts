e36ddb048b7692105b47ec0c4ae87d4185ecdf91
Ansat Jartybayev <ansatjr@gmail.com>
2022-07-03 00:59:34 +0600

    chore: move user-service to services

--- server/src/modules/user/services/user.service.ts
+++ server/src/modules/user/services/user.service.ts
@@ -2,7 +2,7 @@ import {Injectable} from "@nestjs/common";
 import {InjectRepository} from "@nestjs/typeorm";
 import {Repository} from "typeorm";
 
-import {User} from "./entities";
+import {User} from "../entities";
 
 @Injectable()
 export class UserService {
@@ -11,8 +11,13 @@ export class UserService {
     private readonly repository: Repository<User>,
   ) {}
 
+  async create(partial: Partial<User>): Promise<User> {
+    const entity = this.repository.create(partial);
+
+    return this.repository.save(entity);
+  }
+
   findOne = this.repository.findOne;
-  create = this.repository.create;
   save = this.repository.save;
   update = this.repository.update;
   find = this.repository.find;
