6cdf1331ba9d3da6db80a5f2e8abfe560a78dd63
Ansat Jartybayev <ansatjr@gmail.com>
2022-07-03 01:43:14 +0600

    feat: add typings of user module

---
+++ server/src/modules/user/lib/typings.ts
@@ -0,0 +1,13 @@
+export interface UserInterim {
+  matchId: string | null;
+  lobbyId: string | null;
+}
+
+export type RelationshipStatus =
+  | "FRIEND_REQ: 1-2"
+  | "FRIEND_REQ: 2-1"
+  | "FRIENDS"
+  | "BLOCKED: 2-1"
+  | "BLOCKED: 1-2"
+  | "BLOCKED"
+  | "NONE";
