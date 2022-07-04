ceb112a1d84f57a281e7e89a2977f47be9c06a43
Ansat Jartybayev <ansatjr@gmail.com>
2022-07-04 18:59:36 +0600

    chore: update export of match module

--- server/src/modules/match/index.ts
+++ server/src/modules/match/index.ts
@@ -1,1 +1,4 @@
 export {MatchModule} from "./match.module";
+export {MatchPlayerService} from "./services";
+export {Match, MatchPlayer} from "./entities";
+export {MatchPublic, MatchPlayerPublic} from "./lib/typings";
