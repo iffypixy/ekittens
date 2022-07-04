9364f160b67f64d02505aa8082443c2747e3c34e
Ansat Jartybayev <ansatjr@gmail.com>
2022-07-04 19:04:45 +0600

    chore: update user module

--- server/src/modules/user/user.module.ts
+++ server/src/modules/user/user.module.ts
@@ -3,11 +3,13 @@ import {TypeOrmModule} from "@nestjs/typeorm";
 
 import {Relationship, User} from "./entities";
 import {UserService, RelationshipService} from "./services";
+import {UserController} from "./user.controller";
 import {UserGateway} from "./user.gateway";
 
 @Module({
   imports: [TypeOrmModule.forFeature([User, Relationship])],
   providers: [UserService, RelationshipService, UserGateway],
+  controllers: [UserController],
   exports: [UserService],
 })
 export class UserModule {}
