04bb6310f759eeaae8ef652c1bb31b6c8c46899c
Ansat Jartybayev <ansatjr@gmail.com>
2022-07-05 17:13:26 +0600

    chore: update application

--- client/src/app/application.tsx
+++ client/src/app/application.tsx
@@ -1,7 +1,13 @@
 import * as React from "react";
 
+import {Routes} from "@pages/routes";
 import {GlobalStyles} from "./global-styles";
 
 const styles = <GlobalStyles />;
 
-export const App: React.FC = () => styles;
+export const App: React.FC = () => (
+  <>
+    <Routes />
+    {styles}
+  </>
+);
