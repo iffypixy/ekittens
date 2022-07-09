ffd11d6366857519f5f8f59ef27c59b87fcdf0d9
Ansat Jartybayev <ansatjr@gmail.com>
2022-07-09 23:23:07 +0600

    feat: add branch lib

---
+++ client/src/shared/lib/branch/index.tsx
@@ -0,0 +1,14 @@
+import * as React from "react";
+
+interface BranchProps {
+  if: boolean;
+  children: React.ReactNode;
+}
+
+export const Branch: React.FC<BranchProps> = ({if: so, children}) => {
+  const [then, otherwise] = React.Children.toArray(children);
+
+  const output = so ? then : otherwise;
+
+  return <>{output}</>;
+};
