65581bca340e1006fea27029287a3c1849c2808e
Ansat Jartybayev <ansatjr@gmail.com>
2022-07-10 23:55:48 +0600

    feat: add fullscreen template

---
+++ client/src/shared/ui/templates/fullscreen.tsx
@@ -0,0 +1,17 @@
+import * as React from "react";
+import {styled} from "@mui/material";
+
+interface FullscreenProps {
+  children: React.ReactNode;
+}
+
+export const Fullscreen: React.FC<FullscreenProps> = ({children}) => (
+  <Wrapper>{children}</Wrapper>
+);
+
+const Wrapper = styled("div")`
+  width: 100%;
+  height: 100vh;
+  display: flex;
+  overflow: hidden;
+`;
