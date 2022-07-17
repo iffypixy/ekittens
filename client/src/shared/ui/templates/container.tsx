8b8589bc570662787b048f4c7c9785f17b75e98a
Ansat Jartybayev <ansatjr@gmail.com>
2022-07-17 23:57:50 +0600

    feat: add container template

---
+++ client/src/shared/ui/templates/container.tsx
@@ -0,0 +1,10 @@
+import * as React from "react";
+import {Container as MUIContainer} from "@mui/material";
+
+interface ContainerProps {
+  children: React.ReactNode;
+}
+
+export const Container: React.FC<ContainerProps> = ({children}) => (
+  <MUIContainer maxWidth="lg">{children}</MUIContainer>
+);
