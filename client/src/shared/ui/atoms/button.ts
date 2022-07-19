15d5701efb29bbd29b56056b6c146ff1160722b9
Ansat Jartybayev <ansatjr@gmail.com>
2022-07-19 23:51:23 +0600

    chore: style button

--- client/src/shared/ui/atoms/button.ts
+++ client/src/shared/ui/atoms/button.ts
@@ -1,3 +1,18 @@
-import {styled} from "@mui/material";
+import {styled, Button as MUIButton} from "@mui/material";
 
-export const Button = styled("button")``;
+const disabled = "Mui-disabled";
+
+export const Button = styled(MUIButton)`
+  font-family: "Miriam Libre", sans-serif;
+  font-size: 1.4rem;
+  font-weight: 700;
+  text-transform: uppercase;
+  width: fit-content;
+  border-width: 2px !important;
+  border-radius: 0.5rem;
+  padding: 0.7rem 1.4rem;
+
+  &.${disabled} svg {
+    fill: ${({theme}) => theme.palette.action.disabled};
+  }
+`;
