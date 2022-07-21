069f0db621bfbe8fdc85b9ffe4d4813508f4d5e4
Ansat Jartybayev <ansatjr@gmail.com>
2022-07-21 23:52:39 +0600

    feat: add routes

--- client/src/pages/routes.tsx
+++ client/src/pages/routes.tsx
@@ -1,10 +1,83 @@
 import * as React from "react";
-import {Route, Routes as Switch} from "react-router-dom";
+import {Routes as Switch, Route} from "react-router-dom";
 
+import {PrivateRoute, PublicOnlyRoute} from "@shared/lib/routing";
 import {HomePage} from "./home";
+import {LobbyPage} from "./lobby";
+import {UserPage} from "./user";
+import {MatchPage} from "./match";
+import {SignUpPage} from "./sign-up";
+import {PlayPage} from "./play";
+import {SignInPage} from "./sign-in";
+import {LeaderboardPage} from "./leaderboard";
+import {AboutPage} from "./about";
 
 export const Routes: React.FC = () => (
   <Switch>
     <Route path="/" element={<HomePage />} />
+    <Route
+      path="/@/:username"
+      element={
+        <PrivateRoute>
+          <UserPage />
+        </PrivateRoute>
+      }
+    />
+    <Route
+      path="/lobby"
+      element={
+        <PrivateRoute>
+          <LobbyPage />
+        </PrivateRoute>
+      }
+    />
+    <Route
+      path="/login"
+      element={
+        <PublicOnlyRoute>
+          <SignInPage />
+        </PublicOnlyRoute>
+      }
+    />
+    <Route
+      path="/register"
+      element={
+        <PublicOnlyRoute>
+          <SignUpPage />
+        </PublicOnlyRoute>
+      }
+    />
+    <Route
+      path="/play"
+      element={
+        <PrivateRoute>
+          <PlayPage />
+        </PrivateRoute>
+      }
+    />
+    <Route
+      path="/leaderboard"
+      element={
+        <PrivateRoute>
+          <LeaderboardPage />
+        </PrivateRoute>
+      }
+    />
+    <Route
+      path="/about"
+      element={
+        <PrivateRoute>
+          <AboutPage />
+        </PrivateRoute>
+      }
+    />
+    <Route
+      path="/:id"
+      element={
+        <PrivateRoute>
+          <MatchPage />
+        </PrivateRoute>
+      }
+    />
   </Switch>
 );
