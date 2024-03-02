6316fc3ad01bad3f82278e0557f7ab663319703a
Ansat Euler <iffypixy@gmail.com>
2024-03-02 03:04:40 +0100

    refactor: improved env infra

--- apps/api/src/config/s3.config.ts
+++ apps/api/src/config/s3.config.ts
@@ -4,8 +4,8 @@ export const s3Config = registerAs("s3", () => {
   const env = process.env;
 
   return {
-    accessKey: env.S3_ACCESS_KEY,
-    secretAccessKey: env.S3_SECRET_ACCESS_KEY,
+    accessKey: env.S3_PUBLIC_KEY,
+    secretAccessKey: env.S3_SECRET_KEY,
     bucketName: env.S3_BUCKET_NAME,
   };
 });
--- apps/api/types/env.d.ts
+++ apps/api/types/env.d.ts
@@ -5,15 +5,15 @@ export declare global {
       DB_HOST: string;
       DB_PORT: string;
       DB_USERNAME: string;
-      DB_USERNAME: string;
       DB_PASSWORD: string;
+      DB_NAME: string;
       DB_SYNCHRONIZE: string;
       SESSION_SECRET: string;
       REDIS_HOST: string;
       REDIS_PORT: string;
       CLIENT_ORIGIN: string;
-      S3_ACCESS_KEY: string;
-      S3_SECRET_ACCESS_KEY: string;
+      S3_PUBLIC_KEY: string;
+      S3_SECRET_KEY: string;
       S3_BUCKET_NAME: string;
     }
   }
