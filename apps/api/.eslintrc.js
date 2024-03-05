03026c80d327b2d543f3021914ca9c90fdb4f304
Ansat Euler <iffypixy@gmail.com>
2024-03-06 00:36:16 +0100

    feat: added jsdoc types to eslint cfg

--- apps/api/.eslintrc.js
+++ apps/api/.eslintrc.js
@@ -1,25 +1,21 @@
+/** @type { import("eslint").Linter.Config } */
 module.exports = {
-  plugins: ["@typescript-eslint/eslint-plugin", "prettier"],
+  plugins: ["@typescript-eslint/eslint-plugin"],
   parser: "@typescript-eslint/parser",
   parserOptions: {
     project: "tsconfig.json",
     sourceType: "module",
   },
-  extends: [
-    "plugin:@typescript-eslint/recommended",
-    "plugin:prettier/recommended",
-  ],
+  extends: ["eslint:recommended", "plugin:@typescript-eslint/recommended"],
   root: true,
   env: {
     node: true,
   },
   ignorePatterns: [".eslintrc.js"],
   rules: {
-    "@typescript-eslint/interface-name-prefix": "off",
-    "@typescript-eslint/explicit-function-return-type": "off",
-    "@typescript-eslint/explicit-module-boundary-types": "off",
-    "@typescript-eslint/no-explicit-any": "off",
-    "@typescript-eslint/ban-types": "off",
-    "@typescript-eslint/no-empty-interface": "off",
+    "no-duplicate-imports": 2,
+    "@typescript-eslint/explicit-function-return-type": 2,
+    "@typescript-eslint/explicit-module-boundary-types": 2,
+    "@typescript-eslint/no-explicit-any": 0,
   },
 };
